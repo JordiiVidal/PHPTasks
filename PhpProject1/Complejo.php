123b3c3065de890948324cb6aedd8d4ceced7322
dam <dam@192.168.23.122>
2018-10-24 09:36:24 +0200

    practica numeros Complejos

---
+++ PhpProject1/Complejo.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Complejo {
+    
+    private $a;
+    private $ai;
+    
+    public function __construct(float $a, float $ai) {
+        $this->a = $a;
+        $this->ai = $ai;
+    }
+    function getA() {
+        return $this->a;
+    }
+
+    function getAi() {
+        return $this->ai;
+    }
+
+    function sumar(Complejo $complejo){
+        
+    }
+    function multiplicar(Complejo $complejo){
+        
+    }
+    function igualar(Complejo $complejo){
+        
+    }
+}
+$c = new Complejo();
